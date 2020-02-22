95c3bfb203d86e3d50c692971dae3dfb3532f55e
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-02-22 18:28:32 -0500

    Finish Change Notifier Provider sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wax_app/providers/settings_provider.dart';
 import 'package:flutter_wax_app/screens/home.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,13 +9,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Wax App",
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        accentColor: Colors.deepOrangeAccent,
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => SettingsProvider(),
+          child: MaterialApp(
+        title: "Wax App",
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          accentColor: Colors.deepOrangeAccent,
+        ),
+        home: Home(),
       ),
-      home: Home(),
     );
   }
 }
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,32 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wax_app/providers/settings_provider.dart';
+import 'package:provider/provider.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var settingsProvider = Provider.of<SettingsProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Setting"),
       ),
       body: Column(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text("Units"),
-              DropdownButton<String>(
-                value: "Imperial",
-                onChanged: (String value) {},
-                items: <String>[
-                  "Imperial",
-                  "Metric",
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            ],
+          SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Units"),
+                DropdownButton<String>(
+                  value: settingsProvider.units,
+                  onChanged: (String value) {
+                    settingsProvider.setUnits(value);
+                  },
+                  items: <String>[
+                    "Imperial",
+                    "Metric",
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Wax Lines"),
+                Container(
+                  child: Wrap(
+                    spacing: 5,
+                    runSpacing: 5,
+                    children: <Widget>[
+                      FilterChip(
+                        label: Text(
+                          "Swix",
+                          style: TextStyle(
+                            color: Theme.of(context).accentColor,
+                          ),
+                        ),
+                        selected: (settingsProvider.waxLines.contains("Swix"))
+                            ? true
+                            : false,
+                        onSelected: (bool value) {
+                          if (value == true) {
+                            settingsProvider.addWaxLine("Swix");
+                          } else {
+                            settingsProvider.removeWaxLine("Swix");
+                          }
+                        },
+                      ),
+                      FilterChip(
+                        label: Text(
+                          "Toko",
+                          style: TextStyle(
+                            color: Theme.of(context).accentColor,
+                          ),
+                        ),
+                        selected: (settingsProvider.waxLines.contains("Toko"))
+                            ? true
+                            : false,
+                        onSelected: (bool value) {
+                          if (value == true) {
+                            settingsProvider.addWaxLine("Toko");
+                          } else {
+                            settingsProvider.removeWaxLine("Toko");
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
