99bd53fc11fbe7d30cc85a17362d1578e93200e0
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-03-01 20:41:40 -0500

    stream-provider

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,19 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_wax_app/providers/settings_provider.dart';
 import 'package:flutter_wax_app/screens/settings.dart';
+import 'package:flutter_wax_app/services/firestore_service.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_wax_app/models/report.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var reports = Provider.of<List<Report>>(context);
+    SettingsProvider settings = Provider.of<SettingsProvider>(context);
+    var reports = Provider.of<List<Report>>(context)
+        .where((report) => settings.waxLines.contains(report.line))
+        .toList();
+    FirestoreService _db = FirestoreService();
 
     return Scaffold(
       appBar: AppBar(
@@ -30,12 +36,21 @@ class Home extends StatelessWidget {
         itemBuilder: (context, index) {
           Report report = reports[index];
           return ListTile(
-            leading: Text(report.temp.toString()),
+            leading: (settings.units == "Metric")
+                ? Text(report.temp.toString())
+                : Text((((report.temp) * (9 / 5)) + 32).round().toString() +
+                    "\u0080"),
             title: Text(report.wax),
             subtitle: Text(report.line),
             trailing: Text(formatDate(
                 DateTime.parse(report.timeStamp), [h, ":", nn, " ", am])),
           );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _db.addReport();
         },
       ),
     );
